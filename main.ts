1e49cff25a4d9285bf1debf6e80e9ec956133502
Josiah-Rigg <josiah.rigg@stu.ocsb.ca>
2026-02-19 12:07:05 -0500

    Changed comment to find the perimeter and area of 5cm and 3cm.

--- main.ts
+++ main.ts
@@ -1,8 +1,15 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: XXX
- * Created on: Sep 2020
- * This program ...
+ * Created by: Josiah
+ * Created on: Feb 2026
+ * This program will find the perimeter and area of 5cm and 3cm.
 */
 
-basic.showString('Hello, World!')
+basic.showString('A rectangle has dimensions 5 cm & 3 cm')
+//dimensions
+basic.showString("The perimeter would be: 2(5 + 3) = " + (2 * (5 + 3)) + " cm")
+//perimeter
+basic.pause(1000)
+//area code
+basic.showString('A rectangle has dimensions 5 cm & 3 cm')
+basic.showString('Area: 5x3 =' + (5 * 3) + ' cm^2')
